f598aea1247b461e22a1e69bf5b2e24cb7fc7455
root <root@DESKTOP-RHA5CJR.localdomain>
2024-07-14 08:10:21 -0700

    submit arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply= num1 * num2;
+
+console.log(multiply);
+
+let random=Math.floor(Math.random())*10;
+console.log(Number.isInteger(random));
+
+let num3 = 25;
+let num4 = 7;
+let mod= num3 % num4;
+console.log(mod);
+
+let max = Math.max(1, 20, 3, 4, 5);
+console.log(max);
